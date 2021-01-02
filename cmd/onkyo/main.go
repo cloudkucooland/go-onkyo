ec4fcce36237297b4b2f16485f14ead6362a0d3c
Scot Bontrager <cloudkucooland@gmail.com>
2021-01-02 18:59:18 -0500

    more work

--- cmd/onkyo/main.go
+++ cmd/onkyo/main.go
@@ -14,10 +14,10 @@ func main() {
 	listSources := flag.Bool("list-source", false, "List source")
 	flag.Parse()
 
-    host := *hostP
-    if host == "" {
-        host = "192.168.1.152"
-    }
+	host := *hostP
+	if host == "" {
+		host = "192.168.1.152"
+	}
 
 	if *listSources {
 		for k := range eiscp.SourceByName {
@@ -32,19 +32,106 @@ func main() {
 	defer dev.Close()
 	if *value == "" {
 		switch *param {
+		case "details":
+			resp, err := dev.GetDetails()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Println(resp)
 		case "power":
-			fmt.Println(dev.GetPower())
+			resp, err := dev.GetPower()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Println(resp)
 		case "volume":
-			fmt.Println(dev.GetVolume())
+			resp, err := dev.GetVolume()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Printf("current volume: %d\n", resp)
 		case "source":
 			src, err := dev.GetSource()
-			if err == nil {
-				fmt.Println(eiscp.SourceToName[src])
-			} else {
+			if err != nil {
 				fmt.Println(err)
+				return
 			}
+			if src == "" {
+				fmt.Println("unknown")
+				return
+			}
+			fmt.Println(eiscp.SourceToName[src])
+		case "network":
+			resp, err := dev.GetNetworkStatus()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Printf("network status: %s\n", resp)
+			resp, err = dev.GetNetworkPlayStatus()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Printf("network play status: %s\n", resp)
+			resp, err = dev.GetNetworkInfo()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Printf("network info: %s\n", resp)
+		case "nowplaying":
+			nlt, err := dev.GetNetworkTitle()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Printf("now playing: %+v\n", nlt)
+			list, err := dev.GetNetworkListInfo()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Printf("list: %+v\n", list)
+		case "preset":
+			p, _ := dev.GetPreset()
+			fmt.Printf("preset: %s\n", p)
+		case "temp":
+			temp, _ := dev.GetTempData()
+			fmt.Printf("temp: %s\n", temp)
+		case "test":
+			fwv, _ := dev.GetFirmwareVersion()
+			fmt.Printf("firmware version: %s\n", fwv)
+			dm, _ := dev.GetDisplayMode() // works
+			fmt.Printf("display mode: %s\n", dm)
+			ai, _ := dev.GetAudioInformation() // works
+			fmt.Printf("audio information: %s\n", ai)
+			dim, _ := dev.GetDimmer() // works
+			fmt.Printf("dimmer : %s\n", dim)
+			vi, _ := dev.GetVideoInformation() // works
+			fmt.Printf("video information: %s\n", vi)
+			// fl, _ := dev.GetFLInformation() // fails
+			// fmt.Printf("fl: %s\n", fl)
+			// mr, _ := dev.GetMonitorResolution() // fails
+			// fmt.Printf("monitor resolution: %s\n", mr)
+			// r, _ := dev.GetHDMIOut() // fails
+			// fmt.Printf("hdmi out: %s\n", r)
+			// s, _ := dev.GetISF() // fails
+			// fmt.Printf("ISF: %s\n", s)
+			// s, _ := dev.GetWideVideoMode() // fails
+			// fmt.Printf("Wide Video Mode: %s\n", s)
+			s, _ := dev.GetListeningMode() // works
+			fmt.Printf("Listening Mode: %s\n", s)
 		default:
-			panic("Unknown param")
+			resp, err := dev.GetVolume()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Printf("current volume: %d\n", resp)
 		}
 	} else {
 		switch *param {
@@ -66,6 +153,40 @@ func main() {
 				panic("Unknown source")
 			}
 			fmt.Println(dev.SetSource(src))
+		case "preset":
+			msg, err := dev.SetPreset(*value)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(msg)
+		case "netpreset": // hangs
+			msg, err := dev.SetNetworkPreset(*value)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(msg)
+		case "netsrc": // hangs
+			msg, err := dev.SetNetworkService(*value)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(msg)
+		case "netfav": // hangs
+			msg, err := dev.SetNetworkFavorite(*value)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(msg)
+		case "nja": // turn on/off the network art -- saves bandwidth in my config
+			s, err := strconv.ParseBool(*value)
+			if err != nil {
+				panic(err)
+			}
+			state, err := dev.SetNetworkJacketArt(s)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(state)
 		default:
 			panic("Unknow param")
 		}
